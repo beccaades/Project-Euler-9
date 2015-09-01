de466429284e45ff877fd8527c9e345740caf913
beccaades <rebecca.ades@gmail.com>
2015-09-01 15:37:29 -0400

    fixed

--- Project Euler/ProjectEuler2/config/environment.rb
+++
@@ -1,6 +0,0 @@
-require 'bundler/setup'
-require 'pry'
-
-Bundler.require(:default)
-
-require './lib/even_fibonacci.rb'
--- Project Euler/ProjectEuler2/spec/spec_helper.rb
+++
@@ -1,4 +0,0 @@
-require './config/environment'
-
-RSpec.configure do |config|
-end
